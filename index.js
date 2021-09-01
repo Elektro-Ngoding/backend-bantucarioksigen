20908c9e7d827731217300a9687492ec6c99bfe2
fandilladp <fandilladp@gmail.com>
2021-09-02 02:04:25 +0700

    use coockie parser and validation CAPTCHA

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 let cors = require("cors");
-
 const app = express();
 const port = process.env.PORT || 3005;
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.SERVICE_CLIENT }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const dataOksigen = require("./app/routes/bantucarioksigen");
 const auth = require("./app/routes/auth/index");
 const verifyToken = require("./app/configs/verifyToken");
-const shop = require("./app/routes/shop")
+const shop = require("./app/routes/shop");
+const resCAPTCHA = require("./app/configs/captcha");
 
 app.get("/", (req, res) => {
-  res.send("bantucari oksigen");
+  res.json([{ message: "Welcome to api bantucari oksigen" }]);
 });
+
 app.use("/dataoksigen", dataOksigen);
 app.use("/auth", auth);
 app.use("/verifyToken", verifyToken);
-app.use("/product", shop)
+app.use("/product", shop);
+app.use("/captcha", resCAPTCHA);
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
 });
