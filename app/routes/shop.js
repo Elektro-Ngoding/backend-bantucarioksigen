e718bbf4cf44bc1ea6d07e851935b8a5d1494e3d
fandilladp <fandilladp@gmail.com>
2021-09-02 02:08:51 +0700

    fix bug response json

--- app/routes/shop.js
+++ app/routes/shop.js
@@ -41,12 +41,12 @@ router.post("/", async (req, res) => {
         });
         try {
           const dataOksigenPost = await dataProduct.save();
-          res.status(200).json({
+          res.json({
             status: 200,
             message: "success add new Product",
           });
         } catch (error) {
-          res.status(500).json({
+          res.json({
             message: error,
           });
         }
@@ -67,19 +67,68 @@ router.get("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
+  const pathname_url = req.body.data.image_url;
+  cloudinary.uploader.upload(
+    pathname_url.toString(),
+    {
+      folder: req.body.namaToko,
+      resource_type: "image",
+    },
+    async (err, image) => {
+      if (err) {
+        return JSON.stringify(err);
+      } else {
+        try {
+          const productUpdate = await ShopModel.updateOne(
+            { _id: req.params.id },
+            {
+              id_mitra: req.body.id_mitra,
+              namaToko: req.body.namaToko,
+              data: {
+                public_id: image.public_id,
+                namaBarang: req.body.data.namaBarang,
+                product_price: req.body.data.product_price,
+                image_url: image.url,
+                product_url: req.body.data.product_url,
+                provinsi: req.body.data.provinsi,
+                kota: req.body.data.kota,
+                report: req.body.data.report,
+              },
+            }
+          );
+          res.json({
+            status: 200,
+            message: productUpdate,
+          });
+        } catch (error) {
+          res.json({
+            message: error,
+          });
+        }
+      }
+    }
+  );
+});
+
+router.put("/withOutImages/:id", async (req, res) => {
   try {
-    const productUpdate = await ShopModel.updateOne({
-      id_mitra: req.body.id_mitra,
-      namaToko: req.body.namaToko,
-      data: {
-        namaBarang: req.body.data.namaBarang,
-        image_url: req.body.data.image_url,
-        product_url: req.body.data.product_url,
-        provinsi: req.body.data.provinsi,
-        kota: req.body.data.kota,
-        report: req.body.data.report,
-      },
-    });
+    const productUpdate = await ShopModel.updateOne(
+      { _id: req.params.id },
+      {
+        id_mitra: req.body.id_mitra,
+        namaToko: req.body.namaToko,
+        data: {
+          public_id: req.body.data.public_id,
+          namaBarang: req.body.data.namaBarang,
+          product_price: req.body.data.product_price,
+          image_url: req.body.data.image_url,
+          product_url: req.body.data.product_url,
+          provinsi: req.body.data.provinsi,
+          kota: req.body.data.kota,
+          report: req.body.data.report,
+        },
+      }
+    );
     res.json({
       status: 200,
       message: productUpdate,
