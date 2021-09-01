81c0e9cf726ca0c824ab19a4477fb3190812428f
fandilladp <fandilladp@gmail.com>
2021-09-02 02:06:55 +0700

    new params on conection model

--- app/models/AdminModel.js
+++ app/models/AdminModel.js
@@ -1,37 +1,42 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOURI, { useUnifiedTopology: true }, { useNewUrlParser: true });
-
-const AdminSchema = mongoose.Schema({
-    id_mitra: {
-        type: String,
-    },
-    username: {
-        type: String,
-        required: true,
-        max: 255
-    },
-    password: {
-        type : String,
-        require: true,
-        min: 8,
-        max: 1024
-    },
-    role: {
-        type: String,
-        require: true 
-    },
-    verify: {
-        type: String,
-        require: true
-    },
-    created_date: {
-        type: Date,
-        default: Date.now
-    },
-    updated_date: {
-        type: Date,
-        default: Date.now
-    }
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGOURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
-module.exports = mongoose.model('Auth', AdminSchema)
+const AdminSchema = mongoose.Schema({
+  id_mitra: {
+    type: String,
+  },
+  username: {
+    type: String,
+    required: true,
+    max: 255,
+  },
+  password: {
+    type: String,
+    require: true,
+    min: 8,
+    max: 1024,
+  },
+  role: {
+    type: String,
+    require: true,
+  },
+  verify: {
+    type: String,
+    require: true,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Auth", AdminSchema);
